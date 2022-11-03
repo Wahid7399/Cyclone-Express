cb11f526e59e8caedd393ece5c4ff7183f7f2180
Wahid7399 <muhammadwahid7399@gmail.com>
2022-11-03 20:48:05 +0500

    Initail state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Web Project</h1>
+      <h2>Muhammad Wahid (20F0175)</h2>
+      <h2>Muhammad Anfaal (20F0108)</h2>
+      <h2>Sardar Awab Habib (20F0149)</h2>
     </div>
   );
 }
